bf998a5d061ff74cd1664f808833f07584da1966
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2021-01-04 00:14:51 +0530

    Added blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,102 @@
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Our blockchain class
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        # Genesis/First block
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        # To keep things simple, we'll ask the miners to find a nonce such that resultant hash has 4 zeroes at the beginning.
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            # We used subtraction as it is an asymmetric function so it makes finding the right hash a bit tougher.
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        # Find hash of a block
+        # Convert the block dictionary into a string
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        # We'll check 2 things - whether previous hash matches or not & check the PoW for each block
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+
+# Mining our blockchain
+
+
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    # After mining the block, we'll return this response in JSON format using Postman. So, we need to use a dictionary.
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full blockchain
+
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+
+# Running the app
+app.run(host='0.0.0.0', port=5000)
